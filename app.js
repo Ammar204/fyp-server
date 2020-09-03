d1c5153348463caa3e894a7f0b131d1b8ac19565
Ammar204 <49533389+Ammar204@users.noreply.github.com>
2020-09-03 02:46:21 -0700

    ad

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-const express = require('express')
-const app = express()
-const port = 5000
-const connectMongoose = require("./db").connectMongoose
-connectMongoose()
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const express = require("express");
+const app = express();
+const port = 5000;
+const connectMongoose = require("./db").connectMongoose;
+var bodyParser = require("body-parser");
+//routes
+const ad = require("./routes/ad");
+connectMongoose();
+
+//middlewear
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/ad", ad);
 
 app.listen(port, () => {
-  console.log(`app listening at ${port}`)
-})
+  console.log(`app listening at ${port}`);
+});
--- routes/ad.js
+++ routes/ad.js
@@ -1,11 +1,20 @@
-const express = require('express');
+const express = require("express");
 const router = new express.Router();
+const { newAdVerification } = require("../lib/validate");
 
-router.post('/new',(req,res)=>{
-    
-})
+router.get("/", (req, res) => {
+  res.send("ad");
+});
 
+router.post("/new", async (req, res) => {
+  try {
+    await newAdVerification.validateAsync(req.body);
+  } catch (e) {
+    console.log("from /ad/new catch 14 ", e);
+    res.send("Error");
+  }
 
-
+  
+});
 
 module.exports = router;
